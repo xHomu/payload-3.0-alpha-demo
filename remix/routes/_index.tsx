1484ddc6d9408a58464360ebb9030722ab87607b
xHomu <nobisan@gmail.com>
2024-03-08 16:25:38 -0800

    update remix home route

--- remix/routes/_index.tsx
+++ remix/routes/_index.tsx
@@ -13,27 +13,13 @@ export default function Index() {
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
+          <a href="/admin">Payload Next Admin</a>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
+          <a href="/api/users">Payload Rest API</a>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
+          <a href="/api/graphql-playground">Payload GraphQL</a>
         </li>
       </ul>
     </div>
